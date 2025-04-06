8cdb0052d2e1cfdd9eef9c40bc6f396653542052
Mustafa Al-Janabi <33492087+aljanabim@users.noreply.github.com>
2025-04-06 20:52:15 +0200

    Set default port to 3030 #1

--- examples/example-client2.js
+++ examples/example-client2.js
@@ -1,7 +1,7 @@
 const SignalingChannel = require("./signaling-channel");
 const peerId = "testPeer2";
 // Where the signaling server is hosted, for a local server the port must match the one set in the .env files inside the config directory
-const port = process.env.PORT || 3031;
+const port = process.env.PORT || 3030;
 const signalingServerUrl = "http://localhost:" + port;
 // Token must match the value defined in the .env filed inside the config directory
 const token = "SIGNALING123";
